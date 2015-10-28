406d385b2970410124a67ee171a4f5594cfd4e04
david <david.duran.prieto@gmail.com>
2015-10-28 19:20:21 +0100

    [Actualizacion]

--- client.py
+++ client.py
@@ -5,24 +5,39 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+try:
+    sys.argv[5] =! None
+except ValueError:
+    sys.exit("Usage: client.py ip puerto register sip_address expires_value")
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+# Dirección IP del servidor.
+SERVER = sys.argv[1]
+PORT = sys.argv[2]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
-print("Enviando: " + LINE)
-my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-data = my_socket.recv(1024)
+# Contenido que vamos a enviar
+if sys.argv[3] == 'register':
+    REGISTER = sys.argv[3]
+    DIRECCION = sys.argv[4]
+    expires = sys.argv[5]
+    LINE = 'REGISTER sip:'+DIRECCION+' SIP/2.0\r\n\r\n'+ 'Expires: '+expires+'\r\n\r\n'
+    print("Enviando: " + LINE)
+    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    data = my_socket.recv(1024)
+##else:
+  #  LINE = sys.argv[3:]
+   # print("Enviando: " + LINE)
+    #my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    #data = my_socket.recv(1024)
+
 
 print('Recibido -- ', data.decode('utf-8'))
 print("Terminando socket...")
--- server.py
+++ server.py
@@ -5,27 +5,44 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
-    Echo server class
+    Echo server class  
     """
+    dicc={}
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         self.wfile.write(b"Hemos recibido tu peticion")
+        print (self.client_address)
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             print("El cliente nos manda " + line.decode('utf-8'))
+            deco = line.decode('utf-8')
+            campoexpire = 
+            if  deco[:deco.find(' ')] == REGISTER:
+                if deco[deco.find('Expires: '):] == 0:
+                    del dicc[SERVER]
+                else:
+                    dicc = dicc+{SERVER : DIRECCION}
 
+                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+            
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
 
+    def register2json():
+
+    def json2register():
+
+
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    serv = socketserver.UDPServer(('', sys.argv[1]), SIPRegisterHandler)
     print("Lanzando servidor UDP de eco...")
     serv.serve_forever()
