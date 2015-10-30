0f9ff04121d07d6dd318df13c3aaab96bec9f09a
david <david.duran.prieto@gmail.com>
2015-10-30 16:38:39 +0100

    commit2

--- server.py
+++ server.py
@@ -8,11 +8,10 @@ import socketserver
 import sys
 
 
-class SIPRegisterHandler(socketserver.DatagramRequestHandler):
+class EchoHandler(socketserver.DatagramRequestHandler):
     """
-    Echo server class  
+    Echo server class
     """
-    dicc={}
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
@@ -22,27 +21,13 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             print("El cliente nos manda " + line.decode('utf-8'))
-            deco = line.decode('utf-8')
-            campoexpire = 
-            if  deco[:deco.find(' ')] == REGISTER:
-                if deco[deco.find('Expires: '):] == 0:
-                    del dicc[SERVER]
-                else:
-                    dicc = dicc+{SERVER : DIRECCION}
 
-                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-            
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
 
-    def register2json():
-
-    def json2register():
-
-
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', sys.argv[1]), SIPRegisterHandler)
+    serv = socketserver.UDPServer(('', int(sys.argv[1])), EchoHandler)
     print("Lanzando servidor UDP de eco...")
     serv.serve_forever()
