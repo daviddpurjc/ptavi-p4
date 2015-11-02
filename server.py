2ff27afa271b499a55956926e36212810f4e83ea
david <david.duran.prieto@gmail.com>
2015-11-02 19:03:08 +0100

    commitfinal

--- server.py
+++ server.py
@@ -20,13 +20,14 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     
 
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
+        """ Metodo principal del servidor. """
+        # Comprueba que no tenemos usuarios registrados, y llama al metodo json2registered.
         if self.listas == []:
             self.json2registered()
+        # Escribe dirección y puerto del cliente (de tupla client_address)
         print (self.client_address)
         line = self.rfile.read()
         print("El cliente nos manda " + line.decode('utf-8'))
-            #hacemos las muvis
         deco = line.decode('utf-8')
         self.campoexpire = deco[deco.find('s:')+3:]
         if deco.startswith('REGISTER'):
@@ -39,18 +40,23 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         self.register2json()
 
     def register2json(self):
+        """ Gestionamos los usuarios registrados"""
         expira = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(int(self.campoexpire)+time.time()))
         dicc2 = {'address' : self.client_address[0], 'expires': expira}
+        # Recorremos la lista de listas de usuarios para que si ya existía el usuario, borremos su informacion
+        # que será actualizada con su nuevo valor de expiración.
         for lista in self.listas:
             if lista[0] == self.direccion:
                 self.listas.remove(lista)
         self.listas.append([self.direccion,dicc2])
+        # Comprobamos la expiracion de los usuarios registrados, y si alguno ha caducado lo borramos.
         for lista in self.listas:
             if lista[1]['expires'] <= time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time())):
                 self.listas.remove(lista)
         json.dump(self.listas, open("registered.json",'w'), sort_keys=True, indent=4, separators=(',', ': '))
 
     def json2registered(self):
+        """ Comprueba si existe un fichero json para usarlo como lista de usuarios registrados """
         try:
             self.listas = json.load(open("registered.json",'r'))
             print (self.listas)
